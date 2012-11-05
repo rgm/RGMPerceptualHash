8008450e5ff5e55db260e48fe8ee011a8bfe07bd
Ryan McCuaig <rgm@ryanmccuaig.net>
2012-11-05 09:06:03 -0800

    Initial Commit

---
+++ perceptualhash/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  perceptualhash
+//
+//  Created by Ryan McCuaig on 2012/11/05.
+//  Copyright (c) 2012 Ryan McCuaig. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
